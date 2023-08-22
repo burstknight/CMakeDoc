ebc1cf956fbd888dd9e085377eabcab2a61c583e
burstknight <burstknight@gamil.com>
2023-08-22 19:41:37 +0800

    fix: Fixed some bug for example01
    Fixed some bug for example01.

--- Ch01_Introduction/example_01/src/main.c
+++ Ch01_Introduction/example_01/src/main.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "calcPow.h"
 
 int main(int argc, char** argv){
 	if (argc < 3) {
-		printf("Usage: %s base exponential\n", argv[0]);
+		printf("Usage: %s base exponential\n\tbase\t\tGive a float number as the base.\n\texponential\tGive an integer as the exponential.\n", argv[0]);
 		exit(1);
 	} // End of if-condition
 
 	double dBase = atof(argv[1]);
 	int iExp = atoi(argv[2]);
 
-	printf("%f^%d = %f\n", dBase, iExp, calcPow(dBase, iExp));
+	printf("%.4f ^ %d = %.4f\n", dBase, iExp, calcPow(dBase, iExp));
 
 	return 0;
 } // End of main
