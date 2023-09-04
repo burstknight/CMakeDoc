34e5ebd30f498ba471cfdc63631e10ae351f4a69
burstknight <burstknight@gmail.com>
2023-09-04 14:38:51 +0800

    feat: Add `example_04` into ch02
    Add `example_04` into ch02.

---
+++ Ch02_CMake/example_04/src/calcPow.h
@@ -0,0 +1,14 @@
+#ifndef CALCPOW_H
+#define CALCPOW_H
+
+/**
+ * @brief Calculate pow.
+ *
+ * @param dX: Give a number to calculate the power.
+ * @param iExp: Give the exponential.
+ *
+ * @return 
+ */
+double calcPow(double dX, int iExp);
+
+#endif
