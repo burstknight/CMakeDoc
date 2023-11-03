d7697ea704e921fae73336b32cfc4e8c81896a26
burstknight <burstknight@gmail.com>
2023-11-03 14:39:33 +0800

    remove: Remove all examples in ch02
    - Target:
      Ch02 need use easier examples to show the usage for `cmake`, so all
      original examples in ch02 should be removed.
    
    - Changed:
      Remove all examples in ch02.

--- Ch02_Library/example_10/cmake/ColorPrintConfig.cmake
+++
@@ -1,4 +0,0 @@
-add_library(ColorPrint STATIC IMPORTED)
-
-find_library(ColorPrint_Lib_Path ColorPrint HINTS "${CMAKE_CURRENT_LIST_DIR}/../../")
-set_target_properties(ColorPrint PROPERTIES IMPORTED_LOCATION "${ColorPrint_Lib_Path}")
